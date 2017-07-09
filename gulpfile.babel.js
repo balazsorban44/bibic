c46158f1384a0e5c69077ac74d0abffe7f94a68f
Balázs Orbán <info@balazsorban.com>
2017-07-09 18:34:53 +0200

    Added tools and tasks.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,14 +7,16 @@ import pug from 'gulp-pug'
 import sass from 'gulp-sass'
 import babel from 'gulp-babel'
 
-// Media
+// Minifiers
+import htmlMin from 'gulp-htmlmin'
+import cleanCSS from 'gulp-clean-css'
 import imageMin from 'gulp-image'
 import imageResize from 'gulp-image-resize'
 
 // Utilities
 import jshint from 'gulp-jshint'
 import livereload from 'gulp-livereload'
-
+import webserver from 'gulp-webserver'
 // Constants
 const src = 'src/',
       build = 'docs/'
@@ -25,10 +27,11 @@ const src = 'src/',
 // Preprocessors
 gulp.task('pug', () => {
   gulp.src(path.join(src + 'pug/index.pug'))
-  .pipe(pug({pretty: true}))
+  .pipe(pug())
   .pipe(gulp.dest(build))
   .pipe(livereload())
 })
+
 gulp.task('sass', () => {
   gulp.src(path.join(src + 'sass/main.sass'))
   .pipe(sass().on('error', sass.logError))
@@ -49,23 +52,53 @@ gulp.task('es6', () => {
   .pipe(livereload())
 })
 
-// Media tasks
+// Minifiers
+gulp.task('htmlMin', () => {
+  gulp.src(path.join(src + 'pug/index.pug'))
+  .pipe(pug())
+  .pipe(htmlMin({removeComments: true}))
+  .pipe(gulp.dest(build))
+})
+
+gulp.task('cleanCSS', () => {
+  gulp.src(path.join(src + 'sass/main.sass'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(path.join(build + 'assets/css')))
+    .pipe(cleanCSS({debug: true}, (details) => {
+        console.log(details.name + ' before: ' + details.stats.originalSize/1000 +
+        'kb and after: ' + details.stats.minifiedSize/1000 + 'kb');
+      }))
+    .pipe(gulp.dest(path.join(build + 'assets/css')))
+})
 
 gulp.task('images', () => {
-  gulp.src(path.join(src + 'media/images/**/*'))
+  gulp.src(path.join(src + 'media/images/**/*.{jpg,png,svg}'))
   .pipe(imageResize({
-     width : 1920,
-     upscale : false
+    imageMagick: true,
+    width:2560,
+    height: 2560,
+    upscale : false
    }))
   .pipe(imageMin({
-    jpegoptim: true,
-    jpegRecompress: false,
-     mozjpeg: true,
      guetzli: true,
-     optipng: true
+     optipng: true,
+     svgo: true
   }))
   .pipe(gulp.dest(path.join(build + 'assets/img')))
 })
+
+// Utilites
+
+gulp.task('webserver', () => {
+  gulp.src(build)
+    .pipe(webserver({
+      directoryListing: true,
+      open: true,
+      host: '0.0.0.0',
+      port: 8000
+    }));
+});
+
 
 
 // ----------------Bundled tasks---------------- //
@@ -81,4 +114,5 @@ gulp.task('watch', () => {
 })
 gulp.task('default', ['watch','preprocess'])
 
+gulp.task('minify', ['htmlMin','cleanCSS'])
 module.exports = gulp
