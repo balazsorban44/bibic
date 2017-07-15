e6e2ac70bd4897bb55e69265ad3f458876e40c9d
Balázs Orbán <info@balazsorban.com>
2017-07-15 18:48:30 +0200

    Deploying changes.

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -10,6 +10,7 @@ var init = function init() {
       navList = nav.querySelector('ul'),
       hamburger = document.querySelector('#hamburger'),
       rooms = document.querySelectorAll('.room'),
+      heroTitle = document.querySelector('#hero-title'),
       roomSlider = function roomSlider(roomIndex) {
     var current = 0,
         slides = rooms[roomIndex].querySelectorAll('.room-slider img');
@@ -25,6 +26,7 @@ var init = function init() {
     mobile: function mobile() {
       hamburger.addEventListener('click', function () {
         navList.classList.toggle('hidden');
+        hamburger.classList.toggle('hamburger-active');
       });
 
       navList.addEventListener('click', function () {
@@ -32,16 +34,42 @@ var init = function init() {
       });
     },
     scrolled: function scrolled() {
-      var fromTop = window.pageYOffset;
-      console.log(fromTop);
+      var fromTop = window.pageYOffset,
+          w = window.innerWidth;
+      if (fromTop > w * 9 / 16 / 4) {
+        heroTitle.classList.add('hero-title-fixed');
+      } else {
+        heroTitle.classList.remove('hero-title-fixed');
+      }
+      if (fromTop > w * 9 / 16 - 60) {
+        navList.classList.add('menu-fixed');
+        heroTitle.style.backgroundColor = '#1f1511';
+        heroTitle.querySelector('span').style.color = '#fafafa';
+      } else {
+        heroTitle.style.backgroundColor = '';
+        heroTitle.querySelector('span').style.color = '#1f1511';
+        navList.classList.remove('menu-fixed');
+      }
     },
     desktop: function desktop() {
       navList.classList.remove('hidden');
+      var roomList = navList.querySelector('ul'),
+          roomListToggle = navList.querySelector('.szobak a');
+      roomList.classList.add('roomlist-hidden');
+
+      roomListToggle.addEventListener('click', function (e) {
+        e.preventDefault();
+        roomList.classList.toggle('roomlist-hidden');
+      });
+      roomList.addEventListener('click', function () {
+        roomList.classList.toggle('roomlist-hidden');
+      });
+
       window.onscroll = menu.scrolled;
     }
   };
 
-  if (window.matchMedia('(min-width: 2000px)').matches) {
+  if (window.matchMedia('(min-width: 768px)').matches) {
     menu.desktop();
   } else {
     menu.mobile();
