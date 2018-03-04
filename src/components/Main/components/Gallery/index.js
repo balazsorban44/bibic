9152505c6fe6ab69237f14147f597e439f1c0267
Balázs Orbán <info@balazsorban.com>
2018-03-04 13:47:57 +0100

    :heavy_plus_sign: Add react-slick

--- src/components/Main/components/Gallery/index.js
+++ src/components/Main/components/Gallery/index.js
@@ -1,38 +1,22 @@
-import React from 'react'
+import React, {Component} from 'react'
+import Slider from 'react-slick'
 
+export default class Gallery extends Component {
 
-const Gallery = () => (
-  <section id="galeria">
-    <h2>Galéria</h2>
-    <div className="js_simple" id="gallery">
-      <div className="frame js_frame">
-        <ul className="slides js_slides">
-          <li className="js_slide">
-
-          {/* SLIDER */}
-          </li>
-
-        </ul>
-      </div>
-      <span className="js_prev prev">
-        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 501.5 501.5">
-          <g>
-            <path fill="#FFF" d="M302.67 90.877l55.77 55.508L254.575 250.75 358.44 355.116l-55.77 55.506L143.56 250.75z">
-            </path>
-          </g>
-        </svg>
-        </span>
-        <span className="js_next next">
-        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 501.5 501.5">
-          <g>
-            <path fill="#FFF" d="M199.33 410.622l-55.77-55.508L247.425 250.75 143.56 146.384l55.77-55.507L358.44 250.75z">
-            </path>
-          </g>
-        </svg>
-        </span>
-    </div>
-  </section>
-)
-
-
-export default Gallery
+  render() {
+    return (
+      <section id="galeria">
+      <h2>Galéria</h2>
+      <Slider
+        lazyLoad
+        speed={800}
+        className="gallery-slider"
+      >
+        {Array(30).fill(null).map((e, i) => (
+          <img key={i} className="gallery-slide" alt="" src={`https://bibic-vendeghazak.github.io/web/assets/images/gallery/${i}.jpg`}/>
+        ))}
+      </Slider>
+    </section>
+    )
+  }
+}
