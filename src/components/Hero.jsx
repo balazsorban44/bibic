97f69098095f88b11ba899f147cdf4d33e0a26fd
Balázs Orbán <info@balazsorban.com>
2018-10-17 18:02:33 +0200

    ♻️ refactor Hero
     #21

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,20 @@
 import React, {Component} from 'react'
-import hero1 from '../assets/images/hero/2.jpg'
-import hero2 from '../assets/images/hero/1.jpg'
 import logo from '../assets/icons/logo.png'
 import {Link} from 'react-scroll'
+import {Loading} from './shared/Elements'
 
 export default class Header extends Component {
 
-  state = {isBigScreen: false,
-    Carousel: null}
+  state = {isBigScreen: false, images: []}
 
-  componentDidMount() {
+  async componentDidMount() {
+    const images = await Promise.all([
+      import("../assets/images/hero/1.jpg"),
+      import("../assets/images/hero/2.jpg")
+    ])
+    this.setState({images: images.map(({default: img}) => img)})
     this.handleResize()
     window.addEventListener("resize", this.handleResize, false)
-    import("nuka-carousel").then(({default: Carousel}) => this.setState({Carousel}))
   }
 
   componentWillUnmount() {
@@ -26,37 +28,17 @@ export default class Header extends Component {
 
   getOffset = () => window.innerWidth >= 768 ? -64 : 0
 
+
   render() {
-    const {Carousel} = this.state
+    const {images, isBigScreen} = this.state
     return (
       <section className="hero">
-        {Carousel ?
+        {images.length ?
           <Carousel
-            autoplay
-            autoplayInterval={7500}
-            className="hero-slider"
-            dragging={false}
-            heightMode="max"
-            speed={750}
-            transitionMode="fade"
-            withoutControls
-            wrapAround
-          >
-            <img
-              alt="Hero háttérkép"
-              className="hero-slide"
-              src={hero1}
-            />
-            <img
-              alt="Hero háttérkép"
-              className="hero-slide"
-              src={hero2}
-            />
-          </Carousel> :
-          <img
-            alt="Hero háttérkép"
-            src={hero1}
-          />
+            images={images}
+            isBigScreen={isBigScreen}
+          /> :
+          <Loading fullPage/>
         }
         <span className="hero-slider-overlay"/>
         <a
@@ -76,6 +58,51 @@ export default class Header extends Component {
           to="bemutatkozas"
         />
       </section>
+    )
+  }
+}
+
+
+class Carousel extends Component {
+
+  static defaultProps = {
+    interval: 5000, transition: 1000, images: []
+  }
+
+  state = {active: 0, isBigScreen: false}
+
+  componentDidMount(){
+    this.handleResize()
+    window.addEventListener("resize", this.handleResize, false)
+    setInterval(this.update, this.props.interval)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleResize, false)
+    clearInterval(this.update, this.props.interval)
+  }
+
+
+  handleResize = () => this.setState({isBigScreen: window.innerWidth > 768})
+
+
+  update = () =>
+    this.setState(({active}) => ({active: active+1 < this.props.images.length ? active+1 : 0}))
+
+  render() {
+    const {images, transition} = this.props
+    const {active} = this.state
+    return(
+      <div className="hero-slider">
+        {images.map((src, index) =>
+          <div
+            className={`hero-slide ${active === index ? "hero-slide-active" : ""}`}
+            key={src}
+            style={{transition: `${transition/1000}s opacity`,
+              backgroundImage: `url(${src})`}}
+          />
+        )}
+      </div>
     )
   }
 }
