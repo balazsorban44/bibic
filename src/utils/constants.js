1800176700394c711ed94e9e2bc59a22e807dddb
Balázs Orbán <info@balazsorban.com>
2018-10-22 23:13:03 +0200

    add new constant

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,12 +1,13 @@
 import moment from "moment"
 
-
+const BASE_URL="https://bibicvendeghazak.hu"
 const CLOUD_FUNCTION_BASE_URL = "https://europe-west1-bibic-vendeghazak-api.cloudfunctions.net"
 const TODAY = moment()
 const TOMORROW = TODAY.add(1, "day").startOf("day")
 
 
 export {
+  BASE_URL,
   CLOUD_FUNCTION_BASE_URL,
   TODAY, TOMORROW
 }
