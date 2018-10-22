c5ba7986b396a07ad545e7ccb32e59511dff3f4c
Balázs Orbán <info@balazsorban.com>
2018-10-23 01:11:15 +0200

    ⚡️ better form validation

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -2,17 +2,17 @@ import moment from 'moment'
 import {TOMORROW} from './constants'
 
 
-const nameRe = new RegExp(/[\s.áéíóöőúüűÁÉÍÓÖŐÚÜŰa-zA-Z-]/)
+const nameRe = new RegExp(/[\s.áéíóöőúüűÁÉÍÓÖŐÚÜŰa-zA-Z-]+/)
 const emailRe = new RegExp(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)
-const telRe = new RegExp(/[0-9-+\s]/)
-
+const telRe = new RegExp(/^\+?[0-9-\s]+/)
+const addressRe = new RegExp(/[\s.\-\,/áéíóöőúüűÁÉÍÓÖŐÚÜŰa-zA-Z0-9]+/)
 
 export const valid = {
   roomId: (roomId=0, roomsLength) => (0 < roomId) && (roomId <= roomsLength),
   name: name => nameRe.test(name),
   email: email => emailRe.test(email),
   tel: tel => telRe.test(tel),
-  address: address => typeof address === "string" && address.length,
+  address: address => typeof address === "string" && addressRe.test(address),
   message: message => typeof message === "string",
   messageMin: message => typeof message === "string" && message.length >= 40,
   from: from => moment(from).isAfter(TOMORROW),
@@ -25,7 +25,7 @@ export const valid = {
     children.every(child =>
       Object.keys(child).length === 2 &&
         child.name && child.count &&
-        ["0-6", "6-12"].includes(child.name) && child.count
+        ["0-6", "6-12"].includes(child.name) && child.count >= 0
     )),
   peopleCount: (adults, children, maxPeople) => adults + children.reduce((acc, {count}) => acc+count, 0) <= maxPeople,
   subject: subject => ["eventHall", "fullHouse", "special", "other"].includes(subject)
@@ -44,10 +44,11 @@ export const validateReservation = ({
               !valid.to(to) ? "Legkorábbi távozás holnapután" :
                 !valid.period(from, to) ? "A foglalás legalább egy éjszakát kell, hogy tartalmazzon" :
                   !valid.message(message) ? "Érvénytelen üzenet" :
-                    !valid.adults(adults) ? "Érvénytelen felnőtt" :
-                      !valid.children(children) ? "Érvénytelen gyerek" :
-                        !valid.peopleCount(adults, children, maxPeople) ? "A személyek száma nem haladhatja meg a szoba kapacitását" :
-                          false
+                    !valid.messageMin(message) ? "Túl rövid üzenet (min 40 karakter)" :
+                      !valid.adults(adults) ? "Érvénytelen felnőtt" :
+                        !valid.children(children) ? "Érvénytelen gyerek" :
+                          !valid.peopleCount(adults, children, maxPeople) ? "A személyek száma nem haladhatja meg a szoba kapacitását" :
+                            false
 
 export const validateMessage = ({
   subject, name, email, tel, content, address
@@ -57,7 +58,7 @@ export const validateMessage = ({
       !valid.email(email) ? "Érvénytelen e-mail cím" :
         !valid.tel(tel) ? "Érvénytelen telefonszám" :
           !valid.tel(address) ? "Érvénytelen lakcím" :
-            !valid.messageMin(content) ? "A gyorsabb/pontosabb kommunikáció érdekében az üzenet legalább 40 karaktert kell hogy tartalmazzon." :
+            !valid.messageMin(content) ? "Túl rövid üzenet (min 40 karakter)" :
               false
 
 
