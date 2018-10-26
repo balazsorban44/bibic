e720d47820e245ff33a6ae69a11b59e1bf5abb41
Balázs Orbán <info@balazsorban.com>
2018-10-26 17:39:36 +0200

    ⚡️ make frequently used modules global
    Thanks to the video of @leighhalliday at youtube I discovered this beauty, that prevents me writing frequent imports all the time! #27

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,16 @@
-import {configure} from 'enzyme'
+import React from "react"
+import {
+  configure, mount, shallow
+} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 configure({adapter: new Adapter()})
+import moment from "./lib/moment"
+
+/**
+ * make these available without needing to import them every time in tests
+ * @see https://youtu.be/uo0psyTxgQM?t=903
+ */
+global.React = React
+global.mount = mount
+global.shallow = shallow
+global.moment = moment
