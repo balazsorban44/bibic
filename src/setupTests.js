d788136c2447a0e697f04437ff7854e3db03093c
Balázs Orbán <info@balazsorban.com>
2018-10-28 22:14:22 +0100

    ➕ add firebase-mock
     #27

--- src/setupTests.js
+++ src/setupTests.js
@@ -6,6 +6,28 @@ import Adapter from 'enzyme-adapter-react-16'
 configure({adapter: new Adapter()})
 import moment from "./lib/moment"
 import fetch from "jest-fetch-mock"
+import firebasemock from 'firebase-mock'
+
+
+const mockauth = new firebasemock.MockAuthentication()
+const mockdatabase = new firebasemock.MockFirebase()
+const mockfirestore = new firebasemock.MockFirestore()
+const mockstorage = new firebasemock.MockStorage()
+const mockmessaging = new firebasemock.MockMessaging()
+
+const firebase = new firebasemock.MockFirebaseSdk(
+  // use null if your code does not use RTDB
+  (path) => path ? mockdatabase.child(path) : mockdatabase,
+  // use null if your code does not use AUTHENTICATION
+  () => mockauth,
+  // use null if your code does not use FIRESTORE
+  () => mockfirestore,
+  // use null if your code does not use STORAGE
+  () => mockstorage,
+  // use null if your code does not use MESSAGING
+  () => mockmessaging
+)
+
 
 /**
  * make these available without needing to import them every time in tests
@@ -16,3 +38,4 @@ global.mount = mount
 global.shallow = shallow
 global.moment = moment
 global.fetch = fetch
+global.mockFirebase = firebase
