d20986d08671a34688f57ec54a0a3ee6d0fa2558
Balázs Orbán <info@balazsorban.com>
2018-12-17 22:00:32 +0100

    ✅ update tests
     #53

--- src/components/__tests__/Services.test.js
+++ src/components/__tests__/Services.test.js
@@ -1,4 +1,4 @@
-import Services, {ServiceItem} from "../Services"
+import Services from "../Services"
 
 describe("Services component", () => {
   const wrapper = shallow(<Services/>)
@@ -6,52 +6,5 @@ describe("Services component", () => {
   it("renders correctly", () => {
     expect(wrapper).toMatchSnapshot()
   })
-
-})
-
-describe("ServiceItem component", () => {
-  const props = {
-    SIZE_1024: "SIZE_1024.jpg",
-    SIZE_1440: "SIZE_1440.jpg",
-    SIZE_640: "SIZE_640.jpg",
-    desc: "desc",
-    title: "title"
-  }
-  const wrapper = shallow(<ServiceItem {...props}/>)
-
-  it("renders correctly", () => {
-    expect(wrapper).toHaveLength(1)
-  })
-
-
-  it("renders description", () => {
-    expect(wrapper.find("p").prop("children")).toBe(props.desc)
-  })
-
-  it("renders default img", () => {
-    expect(wrapper.find("img").prop("src")).toBe(props.SIZE_640)
-  })
-
-  describe("Img", () => {
-    it("alt is description", () => {
-      expect(wrapper.find("img").prop("alt")).toBe(props.desc)
-    })
-    it("default alt is title", () => {
-      wrapper.setProps({desc: ""})
-      expect(wrapper.find("img").prop("alt")).toBe(props.title)
-    })
-  })
-
-  describe("sources", () => {
-    it("source 1", () => {
-      expect(wrapper.findWhere(e => e.type() === "source" && e.prop("srcSet") === props.SIZE_1024)).toHaveLength(1)
-    })
-
-    it("source 2", () => {
-      expect(wrapper.findWhere(e => e.type() === "source" && e.prop("srcSet") === props.SIZE_1440)).toHaveLength(1)
-    })
-
-  })
-
 
 })
--- src/components/shared/__tests__/Gallery.test.js
+++ src/components/shared/__tests__/Gallery.test.js
@@ -1,6 +1,41 @@
-import {Gallery, GalleryItem} from "../Gallery"
+import {
+  Gallery, GalleryItem, GalleryImage
+} from "../Gallery"
 import {Loading} from "../Elements"
 
+
+describe("GalleryImage component", () => {
+  const props = {alt: "alt",
+    sizes: {
+      SIZE_640: "SIZE_640.jpg",
+      SIZE_1024: "SIZE_1024.jpg",
+      SIZE_1440: "SIZE_1440.jpg"
+    }}
+
+  const wrapper = shallow(<GalleryImage {...props}/>)
+
+  describe("source images are defined", () => {
+
+    it("<960px", () => {
+      expect(wrapper.findWhere(e => e.prop("srcSet") === props.sizes.SIZE_1024).length).toBe(1)
+    })
+
+    it("<1280px", () => {
+      expect(wrapper.findWhere(e => e.prop("srcSet") === props.sizes.SIZE_1440).length).toBe(1)
+    })
+
+    describe("default image", () => {
+      it("has src", () => {
+        expect(wrapper.find("img").prop("src")).toBe(props.sizes.SIZE_640)
+      })
+
+      it("alt is defined", () => {
+        expect(wrapper.find("img").prop("alt")).toBe(props.alt)
+      })
+    })
+  })
+
+})
 
 describe("GalleryItem component", () => {
   const props = {
@@ -16,26 +51,9 @@ describe("GalleryItem component", () => {
     expect(wrapper).toMatchSnapshot()
   })
 
-  describe("source images are defined", () => {
-    it("<960px", () => {
-      expect(wrapper.findWhere(e => e.prop("srcSet") === props.SIZE_1024).length).toBe(1)
-    })
-    it("<1280px", () => {
-      expect(wrapper.findWhere(e => e.prop("srcSet") === props.SIZE_1440).length).toBe(1)
-    })
-    describe("default image", () => {
-      it("has src", () => {
-        expect(wrapper.find("img").prop("src")).toBe(props.SIZE_640)
-      })
-
-      it("alt is desc", () => {
-        expect(wrapper.find("img").prop("alt")).toBe(props.desc)
-      })
-      it("default alt is title", () => {
-        wrapper.setProps({desc: ""})
-        expect(wrapper.find("img").prop("alt")).toBe(props.title)
-      })
-    })
+  it("no description, only show picture", () => {
+    wrapper.setProps({desc: null})
+    expect(wrapper.find(".img-wrapper").length).toBe(0)
   })
 
 })
