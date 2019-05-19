36be515fa25b6583f2e840fecb87653546a4a766
Balázs Orbán <info@balazsorban.com>
2019-05-19 21:39:54 +0200

    🔧 use staging/beta Firebase 🔥 instance in environments other than prod

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,14 +2,35 @@ import * as firebase from 'firebase/app'
 import "firebase/database"
 import "firebase/firestore"
 
-firebase.initializeApp({
-  apiKey: "AIzaSyB4-Y2_RCdrOouJJxUJkBBXGyj4hNdjDs0",
-  authDomain: "bibic-vendeghazak-api.firebaseapp.com",
-  databaseURL: "https://bibic-vendeghazak-api.firebaseio.com",
-  projectId: "bibic-vendeghazak-api",
-  storageBucket: "bibic-vendeghazak-api.appspot.com",
-  messagingSenderId: "586582307718"
-})
+
+// Staging/test/dev
+let config = {
+  apiKey: "AIzaSyBYH1erXxdf3W_vQ-b7PRoA5oiK3mYsno0",
+  authDomain: "bibic-vendeghazak-web-beta.firebaseapp.com",
+  databaseURL: "https://bibic-vendeghazak-web-beta.firebaseio.com",
+  projectId: "bibic-vendeghazak-web-beta",
+  storageBucket: "bibic-vendeghazak-web-beta.appspot.com",
+  messagingSenderId: "456228212717",
+  appId: "1:456228212717:web:19b58c6bdc4ef10c"
+}
+
+
+// Production
+if (
+  "location" in window &&
+  "bibicvendeghazak.hu" === window.location.hostname
+) {
+  config = {
+    apiKey: "AIzaSyB4-Y2_RCdrOouJJxUJkBBXGyj4hNdjDs0",
+    authDomain: "bibic-vendeghazak-api.firebaseapp.com",
+    databaseURL: "https://bibic-vendeghazak-api.firebaseio.com",
+    projectId: "bibic-vendeghazak-api",
+    storageBucket: "bibic-vendeghazak-api.appspot.com",
+    messagingSenderId: "586582307718"
+  }
+}
+
+firebase.initializeApp(config)
 
 
 export const FS = firebase.firestore()
