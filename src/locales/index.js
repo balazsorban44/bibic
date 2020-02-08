e3169eb035be7f20c5bd73d9382ce5da058302e8
Balázs Orbán <info@balazsorban.com>
2020-02-08 10:34:42 +0100

    feat(i18n): add initial translations for 🇭🇺, 🇩🇪 and 🇬🇧

---
+++ src/locales/index.js
@@ -0,0 +1,10 @@
+import huTranslation from "./hu/translation.json"
+import enTranslation from "./en/translation.json"
+import deTranslation from "./de/translation.json"
+
+
+export default {
+  hu: {translation: huTranslation},
+  en: {translation: enTranslation},
+  de: {translation: deTranslation}
+}
