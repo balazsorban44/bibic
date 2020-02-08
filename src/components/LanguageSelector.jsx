804289d64bc439603706ea164b23692279271036
Balázs Orbán <info@balazsorban.com>
2020-02-08 10:35:17 +0100

    feat(i18n): add language selector to menu

---
+++ src/components/LanguageSelector.jsx
@@ -0,0 +1,52 @@
+import React, {useEffect, useRef} from 'react'
+import {useTranslation} from 'react-i18next'
+import FlagsSelect from "react-flags-select"
+import "./language-selector.sass"
+import "react-flags-select/css/react-flags-select.css"
+import {PREFERRED_LANGUAGE} from 'utils/language'
+
+
+const getCountry = () => {
+  let country = PREFERRED_LANGUAGE().toUpperCase()
+  if (country === "EN") {
+    country = "GB"
+  }
+  return country
+}
+
+const LanguageSelector = () => {
+  const flagSelect = useRef()
+
+  useEffect(() => {
+    flagSelect.current.updateSelected(getCountry())
+  }, [])
+  const i18n = useTranslation()[1]
+
+
+  const handleLanguageChange = countryCode => {
+    let locale = countryCode.toLowerCase()
+    if (locale === "gb") {
+      locale = "en"
+    }
+    i18n.changeLanguage(locale, () => {
+      localStorage.setItem("bb:language", locale)
+    })
+  }
+
+  return(
+    <li className="language-selector">
+      <FlagsSelect
+        className="flags-select"
+        countries={["HU", "GB", "DE"]}
+        customLabels={{HU: "Magyar", GB: "English", "DE": "Deutsch"}}
+        defaultCountry={getCountry()}
+        onSelect={handleLanguageChange}
+        optionsSize={22}
+        ref={flagSelect}
+        selectedSize={22}
+      />
+    </li>
+  )
+}
+
+export default LanguageSelector
