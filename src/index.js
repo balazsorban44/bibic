e83a4df350b2c793616bd18891487c46e1a097ca
Balázs Orbán <info@balazsorban.com>
2020-02-08 10:39:16 +0100

    feat: resolve paths

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import {BrowserRouter} from 'react-router-dom'
 import {render} from 'react-dom'
+import {BrowserRouter} from 'react-router-dom'
+
 import "lib/i18n"
 import {NotificationProvider} from 'lib/notification'
 import 'react-toastify/dist/ReactToastify.css'
 
 import './sass/main.sass'
 import Database from 'components/db'
-import Database from './components/db'
-import App from './components/App'
+import App from 'components/App'
 
 render(
   <BrowserRouter>
     <NotificationProvider>
-    <Database>
-      <App/>
-    </Database>
+      <Database>
+        <App/>
+      </Database>
     </NotificationProvider>
   </BrowserRouter>,
   document.querySelector('.app')
