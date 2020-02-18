1a3b78f5bf99d28b3062daa508c7136143375cb6
Balázs Orbán <info@balazsorban.com>
2020-02-18 19:56:57 +0100

    chore(env): extract variables to .env and config

---
+++ src/utils/env.js
@@ -0,0 +1,42 @@
+const config = {
+  "app": {
+    "DOMAIN": process.env.REACT_APP_DOMAIN,
+    "VERSION": process.env.REACT_APP_VERSION,
+    "YEAR": new Date().getFullYear(),
+    "DEFAULT_LOCALE": process.env.DEFAULT_LOCALE,
+    "locales": ["hu", "en", "de"]
+  },
+  "firebase": {
+    "initConfig": {
+      "apiKey": process.env.REACT_APP_API_KEY,
+      "authDomain": process.env.REACT_APP_AUTH_DOMAIN,
+      "databaseURL": process.env.REACT_APP_DATABASE_URL,
+      "projectId": process.env.REACT_APP_PROJECT_ID,
+      "storageBucket": process.env.REACT_APP_STORAGE_BUCKET,
+      "messagingSenderId": process.env.REACT_APP_MESSAGING_SENDER_ID
+    },
+    "CLOUD_FUNCTION_URL": process.env.REACT_APP_CLOUD_FUNCTION_URL
+  },
+
+  "googleMaps": {
+    "API_KEY": process.env.REACT_APP_MAP_API_KEY,
+    "URL": process.env.REACT_APP_MAP_URL,
+    "zoom": parseInt(process.env.REACT_APP_MAP_ZOOM, 10),
+    "center": {
+      "lat": parseFloat(process.env.REACT_APP_MAP_LAT, 10),
+      "lng": parseFloat(process.env.REACT_APP_MAP_LONG, 10)
+    }
+  },
+  "contact": {
+    "ADDRESS": process.env.REACT_APP_ADDRESS,
+    "TEL_SHORT": process.env.REACT_APP_TEL_SHORT,
+    "TEL_LONG": process.env.REACT_APP_TEL_LONG,
+    "FACEBOOK_URL": process.env.REACT_APP_FACEBOOK_URL,
+    "INSTAGRAM_URL": process.env.REACT_APP_INSTAGRAM_URL,
+    "YOUTUBE_URL": process.env.REACT_APP_YOUTUBE_URL,
+    "GERGO_BOOS_URL": process.env.REACT_APP_BOOS_GERGO_URL,
+    "BALAZS_ORBAN_URL": process.env.REACT_APP_BALAZS_ORBAN_URL
+  }
+}
+
+export default config
